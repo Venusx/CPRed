6db9931c3548d64859c77548b4ce23ca7912fc56
Vanessa C <vanessahuggins09@gmail.com>
2020-02-16 01:56:23 -0500

    Updated role

---
+++ _js/player.js
@@ -0,0 +1,26 @@
+var roleArray = ["Rockerboy", "Solo", "Netrunner", "Tech", "Media", "Lawmen", "Exec", "Fixer", "Nomad"];
+var roleInfo = ["Rock and roll rebels who use performance and rhetoric to fight authority.<br>If you live to rock, this is where you belong. As a Rockerboy, you’re one of the street poets, the social conscience, and a rebel of the Age of The Red.", 
+				"Assassins, bodyguards, killers, and soldiers for hire in a lawless new world.",
+				"Cybernetic master hackers of the post–NET world and brain burning secret stealers.",
+				"Renegade mechanics and doctors patching up meat and metal alike.",
+				"Newsmen, media stars, and social influencers risking it all for the truth.<br>*Not Available",
+				"Maximum lawmen patrolling the mean streets and road warrior highways beyond.<br>*Not Available",
+				"Corporate power brokers and business raiders fighting to restore the rule of the Megacorps.<br>*Not Available",
+				"Deal makers, smugglers, organizers, and information brokers on the post–war streets of the future.",
+				"Transportation experts and ultimate road warriors."];
+
+var p1RoleChoice;
+
+for (i = 0; i < roleArray.length; i++) {
+	document.getElementById("roleChoice").innerHTML += "<p onmouseover='roleInfoChoice(" + i + ")' onclick='p1Role(" + i + ")'>" + (i+1) + ": " + roleArray[i] + " " + "</p>";
+}
+
+function p1Role(x) {
+	p1RoleChoice = roleArray[x];
+	document.getElementById("roleChoice").innerHTML = "You Chose: " + p1RoleChoice;
+}
+
+function roleInfoChoice(x) {
+	roleMouse = roleInfo[x];
+	document.getElementById("roleInfoHover").innerHTML = roleMouse;
+}
--- _js/splashscreen.js
+++ _js/splashscreen.js
@@ -1,6 +1,9 @@
 var quotes = [
-	"Have you ever seen the sunlight/ Screaming fast and mean and low? Get yourself  illuminated/ Get yourself that cyber glow. Made of steel and hard emotion/ Give my love and face the day/ In your face and here to party. Never gonna fade away.<br><p align='right'> -Johnny Silverhand</p>",
-	"So welcome to the Time of the Red, choombatta. It's more dangerous than the once glamorous and hyper charged Lost Time of the  Cyberpunk 2020s. The body count is higher and the stakes even  nastier. But that's not  going to stop you, is it? Didn't think so.<br><p align='right'> -Trace Santiago</p>",
+	"The Rules:<br>1) Style Over Substance. 2) Attitude is Everything. 3) Always take It to the Edge. 4) Break the Rules.<br><p align='right'>RIPPERJACK",
+	"Always take it to the Edge. It's the Cyberpunk way.<br><p align='right'> Johnny Silverhand",
+	"I'm not a man or a machine. I'm just something in between. I'm all love—a dynamo So push the button and let me go.<br><p align='right'> —Lovin' Every Minute of It<br> 1984 Zomba Enterprises Inc. (ASCAP)</p>",
+	"Have you ever seen the sunlight/ Screaming fast and mean and low? Get yourself illuminated. Get yourself that cyber glow. Made of steel and hard emotion. Give my love and face the day. In your face and here to party. Never gonna fade away.<br><p align='right'> -Johnny Silverhand</p>",
+	"So welcome to the Time of the Red, choombatta. It's more dangerous than the once glamorous and hyper charged Lost Time of the Cyberpunk 2020s. The body count is higher and the stakes even nastier. But that's not going to stop you, is it? Didn't think so.<br><p align='right'> -Trace Santiago</p>",
 	"All Right pipsqueak, time to see if metal really is better than meat. let's dance.<br><p align='right'> -Morgan Blackhand</p>",
 	"Pay attention, punks. My old man used to say History will bite you in the ass if you don’t learn about it. It’s like a rabid dog that will go after you if you keep going back to its hideout. So, I’m going to fill you in with some history you’d better learn pronto, now that you’re on the Street. Because you do not want to get bit by this beast. <br><p align='right'> –Trace Santiago</p>"
 ]
