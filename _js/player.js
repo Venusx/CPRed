4c792ec3b700510a36d96557d16c4383b482ce0c
Vanessa C <vanessahuggins09@gmail.com>
2020-02-20 01:46:08 -0500

    CSS update

--- _js/player.js
+++ _js/player.js
@@ -1,18 +1,18 @@
 var roleArray = ["Rockerboy", "Solo", "Netrunner", "Tech", "Media", "Lawmen", "Exec", "Fixer", "Nomad"];
-var roleInfo = ["Rock and roll rebels who use performance and rhetoric to fight authority.<br>If you live to rock, this is where you belong. As a Rockerboy, you’re one of the street poets, the social conscience, and a rebel of the Age of The Red.", 
-				"Assassins, bodyguards, killers, and soldiers for hire in a lawless new world.<br>You were re-born with a gun in your hand—the flesh and blood hand, not the metallic weapons factory that covers most of your other arm.",
-				"Cybernetic master hackers of the post–NET world and brain burning secret stealers.",
-				"Renegade mechanics and doctors patching up meat and metal alike.",
-				"Newsmen, media stars, and social influencers risking it all for the truth.<br>*Not Available",
-				"Maximum lawmen patrolling the mean streets and road warrior highways beyond.<br>*Not Available",
-				"Corporate power brokers and business raiders fighting to restore the rule of the Megacorps.<br>*Not Available",
-				"Deal makers, smugglers, organizers, and information brokers on the post–war streets of the future.",
-				"Transportation experts and ultimate road warriors.<br>Years ago, the Corps drove your family off the Farm. They rolled in, took over the land, and put renta-cops all over the place. But that was Before the War."];
+var roleInfo = ["<p>Rock and roll rebels who use performance and rhetoric to fight authority.<br></p><p>If you live to rock, this is where you belong. As a Rockerboy, you’re one of the street poets, the social conscience, and a rebel of the Age of The Red.</p>", 
+				"<p>Assassins, bodyguards, killers, and soldiers for hire in a lawless new world.<br></p><p>You were re-born with a gun in your hand—the flesh and blood hand, not the metallic weapons factory that covers most of your other arm.</p>",
+				"<p>Cybernetic master hackers of the post–NET world and brain burning secret stealers.</p>",
+				"<p>Renegade mechanics and doctors patching up meat and metal alike.</p>",
+				"<p>Newsmen, media stars, and social influencers risking it all for the truth.<br></p><p>*Not Available</p>",
+				"<p>Maximum lawmen patrolling the mean streets and road warrior highways beyond.<br></p><p>*Not Available</p>",
+				"<p>Corporate power brokers and business raiders fighting to restore the rule of the Megacorps.<br></p><p>*Not Available</p>",
+				"<p>Deal makers, smugglers, organizers, and information brokers on the post–war streets of the future.</p>",
+				"<p>Transportation experts and ultimate road warriors.<br></p><p>Years ago, the Corps drove your family off the Farm. They rolled in, took over the land, and put renta-cops all over the place. But that was Before the War.</p>"];
 
 var p1RoleChoice;
 
 for (i = 0; i < roleArray.length; i++) {
-	document.getElementById("roleChoice").innerHTML += "<p onmouseover='roleInfoChoice(" + i + ")' onclick='p1Role(" + i + ")'>" + (i+1) + ": " + roleArray[i] + " " + "</p>";
+	document.getElementById("roleChoice").innerHTML += "<p onmouseover='roleInfoChoice(" + i + ")' onclick='p1Role(" + i + ")'>" + roleArray[i] + " " + "</p>";
 }
 
 function p1Role(x) {
--- _js/site.js
+++ _js/site.js
@@ -6,10 +6,10 @@ function rollDice(i) {
 }
 
 //Header
-document.getElementById("header").innerHTML = '<h1 class="siteLogo">Cyberpunk</h1><h2 class="subLogo">Character Creator</h2><ul class="menuTop"><li class="btn"><a href="../index2.html">Home</a></li><li class="btn"><a href="about.html">About</a></li><li class="btn"><a href="contact.html">Contact</a></li></ul>';
+document.getElementById("header").innerHTML = '<h1 class="siteLogo">Cyberpunk</h1><h2 class="subLogo">RED</h2><ul class="menuTop"><li class="btn"><a href="../index2.html">Home</a></li><li class="btn"><a href="about.html">About</a></li><li class="btn"><a href="contact.html">Contact</a></li></ul>';
 //Button
 function refreshPage(){
     window.location.reload();
 } 
 //Footer
-document.getElementById("footer").innerHTML = "Site Made by Venus(Git, Email, Twitter) Cyberpunk 2.0.2.0. is owned by R. Talsorian Games.";
+document.getElementById("footer").innerHTML = "Site Made by Venus(Git, Email, Twitter) Cyberpunk RED. is owned by R. Talsorian Games.";
